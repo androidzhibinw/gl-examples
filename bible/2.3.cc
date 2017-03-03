40f5885fb30db61c765c28fefb04596945aae845
android_zhibinw <android_zhibinw@163.com>
2017-03-03 17:46:24 +0800

    using glsl. the version suck using glxinfo to check what gl version your system support. using error check to find the real msg.

---
+++ bible/2.3.cc
@@ -0,0 +1,131 @@
+
+//g++ 2.3.cc -lGL -lglut -lGLEW -o 2.3
+
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+GLuint rendering_program;
+GLuint vertex_array_object;
+
+GLenum err;
+
+void check_glError(void)
+{
+    while((err = glGetError()) != GL_NO_ERROR)
+    {
+        printf("GL error is %d",err);
+    }
+}
+void checkShaderStatus(GLuint shader,const char* msg)
+{
+    GLint val = GL_FALSE;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &val);
+    if( val != GL_TRUE)
+    {
+        printf("shader status wrong in %s",msg);
+    }
+}
+
+GLuint compile_shaders(void)
+{
+    GLuint vertex_shader;
+    GLuint fragment_shader;
+    GLuint program;
+    //source for vetex shader
+    static const GLchar * vertex_shader_source []= 
+    {
+        "void main()                                \n"
+        "{                                           \n"
+        "    gl_Position = vec4(0.0, 0.0, 0.5, 1.0); \n"
+        "}                                           \n"
+    };
+    // source for fragement shader
+    static const GLchar * fragment_shader_source []= 
+    {
+        "void main()                                 \n"
+        "{                                           \n"
+        "   gl_FragColor = vec4(0.0, 0.8, 1.0, 1.0); \n"
+        "}                                           \n"
+    };
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+    checkShaderStatus(vertex_shader,"vertex compiling\n");
+    // Create and compile fragment shader
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+    checkShaderStatus(fragment_shader,"fragement compiling\n");
+    // Create program, attach shaders to it, and link it
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    // Delete the shaders as the program has them now
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    return program;
+}
+
+static void GLRender()
+{
+    static const GLfloat red [] = {0.5f,0.0f,0.0f,1.0f};
+    glClearBufferfv(GL_COLOR, 0, red);
+    glUseProgram(rendering_program);
+    glPointSize(40.0f);
+    glDrawArrays(GL_POINTS, 0, 1);
+    check_glError();
+
+}
+void startUp()
+{
+    rendering_program = compile_shaders();
+    glGenVertexArrays(1, &vertex_array_object);
+    glBindVertexArray(vertex_array_object);
+}
+
+void shutDown()
+{
+    glDeleteVertexArrays(1, &vertex_array_object);
+    glDeleteProgram(rendering_program);
+    glDeleteVertexArrays(1, &vertex_array_object);
+}
+
+
+static void Render()
+{
+    GLRender();
+    glFlush();
+    glutSwapBuffers();
+    //glutPostRedisplay();
+}
+
+
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(Render);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DEPTH| GLUT_DOUBLE|GLUT_RGBA);
+    glutInitContextVersion(3,0);
+    glutInitContextFlags(GLUT_DEBUG);
+    glutInitWindowSize(1024,768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("sample t001");
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+      fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    }
+    startUp();
+    InitializeGlutCallbacks();
+    glutMainLoop();
+    shutDown();
+    return 0;
+
+}
